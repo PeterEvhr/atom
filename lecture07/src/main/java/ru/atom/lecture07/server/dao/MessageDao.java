0880b007bced6c3251c043368422324cfa6491b8
PeterEvhr <peter.evhr@gmail.com>
2020-02-12 16:12:40 +0200

    l06

--- lecture07/src/main/java/ru/atom/lecture07/server/dao/MessageDao.java
+++ lecture07/src/main/java/ru/atom/lecture07/server/dao/MessageDao.java
@@ -1,4 +1,14 @@
 package ru.atom.lecture07.server.dao;
 
+import ru.atom.lecture07.server.model.User;
+import ru.atom.lecture07.server.model.Message;
+
+import java.util.List;
+
 public interface MessageDao {
+    User getByUser(User user);
+
+    void save(Message message);
+
+    List<Message> findAll();
 }
---
+++ lecture07/src/main/java/ru/atom/lecture07/server/dao/MessageDaoImpl.java
@@ -0,0 +1,46 @@
+package ru.atom.lecture07.server.dao;
+
+import ru.atom.lecture07.server.model.Message;
+import ru.atom.lecture07.server.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class MessageDaoImpl implements MessageDao{
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public User getByUser(User user) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Message> criteria = builder.createQuery(Message.class);
+        Root<Message> from = criteria.from(Message.class);
+        criteria.select(from);
+        criteria.where(builder.equal(from.get("user_id"), user.getId()));
+        TypedQuery<Message> typed = em.createQuery(criteria);
+        Message message;
+        try {
+            message = typed.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+        return user;
+    }
+
+    @Override
+    public void save(Message message) {
+        em.persist(message);
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return em.createQuery("Select t from " + Message.class.getSimpleName() + " t").getResultList();
+    }
+}
+
